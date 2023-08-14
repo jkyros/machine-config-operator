8d3309f8fae03d8d0b0690f37465b818a3dd7a7b
John Kyros <79665180+jkyros@users.noreply.github.com>
2023-08-14 15:54:49 -0500

    update machineconfig imports to openshift/api
    This updates our imports to read the API from openshift/api,
    removes the generated and API packages (because client-go supersedes
    them) and moves the api helper functions to their own package
    "apihelpers" since they did not go with the API.

---
+++ hack/crds-sync.sh
@@ -0,0 +1,29 @@
+ #!/usr/bin/env bash
+
+set -euo pipefail
+
+# map names of CRD files between the vendored openshift/api repository and the ./install directory
+CRDS_MAPPING=( "v1/0000_80_containerruntimeconfig.crd.yaml:0000_80_machine-config-operator_01_containerruntimeconfig.crd.yaml"
+               "v1/0000_80_kubeletconfig.crd.yaml:0000_80_machine-config-operator_01_kubeletconfig.crd.yaml"
+               "v1/0000_80_machineconfig.crd.yaml:0000_80_machine-config-operator_01_machineconfig.crd.yaml"
+               "v1/0000_80_machineconfigpool.crd.yaml:0000_80_machine-config-operator_01_machineconfigpool.crd.yaml" ) 
+                #TODO(jkyros): 0000_80_machine-config-operator_02_containerruntimeconfig.crd.yaml)
+
+for crd in "${CRDS_MAPPING[@]}" ; do
+    SRC="${crd%%:*}"
+    DES="${crd##*:}"
+    cp "vendor/github.com/openshift/api/machineconfiguration/$SRC" "install/$DES"
+done
+
+
+#v1/0000_10_containerruntimeconfig.crd.yaml:0000_80_machine-config-operator_01_containerruntimeconfig.crd.yaml
+#v1/0000_10_kubeletconfig.crd.yaml:0000_80_machine-config-operator_01_kubeletconfig.crd.yaml
+#v1/0000_10_machineconfig.crd.yaml:0000_80_machine-config-operator_01_machineconfig.crd.yaml
+#v1/0000_10_machineconfigpool.crd.yaml:0000_80_machine-config-operator_01_machineconfigpool.crd.yaml
+#TODO(jkyros): 0000_80_machine-config-operator_02_containerruntimeconfig.crd.yaml
+
+#./vendor/github.com/openshift/api/machineconfiguration/
+#./vendor/github.com/openshift/api/machineconfiguration/
+#./vendor/github.com/openshift/api/machineconfiguration/
+#./vendor/github.com/openshift/api/machineconfiguration/
+#./vendor/github.com/openshift/api/machineconfiguration/v1/
